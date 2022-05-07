6c8a13975479a4ebc37e73f441c10fa055e7e9ad
Qingyang Li <liqingyang1602@163.com>
2022-05-07 14:59:42 +0800

    2nd geo-map for population

--- src/geoMap.js
+++ src/geoMap.js
@@ -2,17 +2,20 @@ import React from "react";
 import { geoPath, geoMercator } from "d3-geo";
 
 export function GeoMap(props) {
-    const {offsetX, offsetY, map, colormap, width, height, data, selyear} = props;
+    // dkey is the column to be visualized
+    const {offsetX, offsetY, map, colormap, width, height, data, dkey} = props;
     let path = geoPath(geoMercator().fitSize([width, height], map));
     //console.log(map.features);
+    //console.log(data);
     return <g transform={`translate(${offsetX}, ${offsetY})`}>
         {map.features.map(feature => {
             const province = data.filter(d => d.Province === feature.properties.NAME_1);
-            const _key = '_' + selyear;
+            //const _key = '_' + selyear;
             if(province[0]) {
+                // province found
                 return <path key={feature.properties.NAME_1 + "boundary"} className={"boundary"}
                   d={path(feature)} opacity={1.0}
-                  style={{fill:colormap(province[0][_key])}}/>;
+                  style={{fill:colormap(province[0][dkey])}}/>;
             }
             else {
                 // in case not found
