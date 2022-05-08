dc4a2c4ebbd34cfea672349bf5a389fa4bfa0d35
Qingyang Li <liqingyang1602@163.com>
2022-05-08 18:35:34 +0800

    add radio buttons for sorting

--- src/barchart.js
+++ src/barchart.js
@@ -3,10 +3,27 @@ import { XAxis, YAxis } from "./axes";
 import { scaleLinear, scaleBand, area, max, min, curveBasis } from "d3";
 
 export function BarChart(props) {
-    const { offsetX, offsetY, data, gdpdata, height, width, selyear, selectedProvince, setSelectedProvince } = props;
+    const { offsetX, offsetY, data, gdpdata, height, width,
+        selyear, selectedProvince, setSelectedProvince, mode } = props;
         const _key = '_' + selyear;
+        // interaction: sorting
+        let scaleData = data;
+        if(mode == "Population") {
+            // sort by population
+            data.sort(function(a, b) {
+                return -(a[_key] - b[_key]);
+            });
+            scaleData = data;
+        }
+        else if(mode == "GDP") {
+            // sort by GDP
+            gdpdata.sort(function(a, b) {
+                return -(a[_key] - b[_key]);
+            });
+            scaleData = gdpdata;
+        }
         const xScale = scaleBand().range([0, width])
-            .domain(data.map(d => d.Province))
+            .domain(scaleData.map(d => d.Province));// xScale related to mode
         const yScale1 = scaleLinear().range([height/2, 0])
             .domain([0, max(data, d => d[_key])])
             .nice();
@@ -43,8 +60,9 @@ export function BarChart(props) {
             <XAxis xScale={xScale} height={height} width={width} />
 
         <g transform={`translate(${0}, ${height/2})`}>
-            <text style={{ textAnchor:'start', fontSize:'15px'}} transform={`translate(${width*2/5}, ${height/2+50})`}>
-            {"GDP per capita"}
+            <text style={{textAnchor:'start', fontSize:'15px'}}
+              transform={`translate(${width * 2/5}, ${height/2 + 100})`}>
+                {"GDP per capita"}
             </text>
             {gdpdata.map( d =>
                 <rect key={d.Province+"barDown"} x={xScale(d.Province)} y={0}
